78dbe38fed5173d2d541d7c8b393e2160aef5060
ammer03 <ammerdomingo@gmail.com>
2024-08-21 00:05:53 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ const plugins = require('./gatsby-config.plugins');
 const defaultLanguage = 'en';
 
 module.exports = {
-    pathPrefix: 'home-finder',
+    pathPrefix: 'HomeSearch',
     siteMetadata: {
         title: 'Home Finder',
         author: 'blopa',
         summary: 'Find homes for you',
         defaultLanguage,
         description: 'Home Finder',
-        siteUrl: 'https://werules.github.io/home-finder/',
+        siteUrl: 'https://ammer03.github.io/HomeSearch/',
         social: {
             twitter: 'thepiratepablo',
         },
